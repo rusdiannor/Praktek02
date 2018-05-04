1e24d16a6a893cd0d39c55d937f848bdbcf1d480
Rusdiannor <34591361+rusdiannor@users.noreply.github.com>
2018-05-04 18:25:40 +0700

    Mengisi nilai atribute

--- src/praktek02/BalokAksi.java
+++ src/praktek02/BalokAksi.java
@@ -3,6 +3,9 @@ package praktek02;
 public class BalokAksi {
     public static void main(String[] args) {
         Balok r1 = new Balok ();
+        r1. panjang = 4 ;
+        r1. lebar  = 5;
+        r1. tinggi  = 3;
     }
            
 }
