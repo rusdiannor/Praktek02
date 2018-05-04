2cdd20dbb1d43247b0411f74469b47e83de2fd10
Rusdiannor <34591361+rusdiannor@users.noreply.github.com>
2018-05-04 18:15:01 +0700

    Membuat Attribute dan Behavior

--- src/praktek02/Balok.java
+++ src/praktek02/Balok.java
@@ -1,5 +1,16 @@
 package praktek02;
-
 public class Balok {
-    
+  double panjang ;
+  double lebar ;
+  double tinggi ;
+  
+    void cetakInfo () {
+       System.out.println("================");
+       System.out.println("Panjang   :"+panjang );
+       System.out.println("Lebar   :"+lebar );
+       System.out.println("Tinggi :"+tinggi);
+       System.out.println("================");
+       
+
+}
 }
